4a5d8781d516f2456ab4d885290bc9bde7d8b172
Robinson Lal <robinsonlal1997@gmail.com>
2026-09-12 13:01:51 +0530

    Improved the code for child Task component for clean data from parent App.js file

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,10 +3,19 @@ import "../components/Task.css"
 
 function Task(props) {
     return (
-        <div id='task-container' >
-            {props.note}
-            <button id='task-button' onClick={props.onDelete} >Delete Task</button>
-        </div>
+        // <div id='task-container' >
+        //     {props.note} 
+
+        //     <button id='task-button' onClick={props.onDelete} >Delete Task</button>
+        // </div>
+
+        <li className="task-item">
+            <span className="task-text">{props.note}</span>
+            <button className="task-delete-button" onClick={props.onDelete}>
+                Delete Task
+            </button>
+        </li>
+
     )
 }
 
